4240548b383befb8fe20129f9caaa26c572ecdc1
ianam <ian.aman4571@gmail.com>
2024-11-05 20:30:39 -0500

    Initial commit

---
+++ Student Projects/company.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+st.header("The Best Company")
+content = ("Lorem Ipsum etc."
+           "")
+st.write(content)
+
+st.subheader("Our Team")
+
+col1, col2, col3 = st.columns(3)
+
+column1 =
+
+st.image
